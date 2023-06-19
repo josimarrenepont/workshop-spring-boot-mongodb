34ba39289608fabc6d3a2948e75b92edd85ace54
jnrenepont@hotmail.com <jnrenepont@hotmail.com>
2023-06-18 22:21:52 -0300

    getting user by id

--- src/main/java/com/josimarrenepont/workshopmongodb/services/UserService.java
+++ src/main/java/com/josimarrenepont/workshopmongodb/services/UserService.java
@@ -1,21 +1,28 @@
 package com.josimarrenepont.workshopmongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.josimarrenepont.workshopmongodb.domain.User;
 import com.josimarrenepont.workshopmongodb.repository.UserRepository;
+import com.josimarrenepont.workshopmongodb.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repository;
-	
-	public List<User> findAll(){
-		return repository.findAll(); 
+
+	public List<User> findAll() {
+		return repository.findAll();
 	}
 
+	public User findById(String id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Not found object"));
+	}
+	
 }
---
+++ src/main/java/com/josimarrenepont/workshopmongodb/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,14 @@
+package com.josimarrenepont.workshopmongodb.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+
+	public ObjectNotFoundException (String msg) {
+		super(msg);
+		
+	}
+	
+	
+
+}
