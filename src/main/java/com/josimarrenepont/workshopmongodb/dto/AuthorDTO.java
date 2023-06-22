141c90f2da4aac6f1284e218bc6ab73365a0464b
jnrenepont@hotmail.com <jnrenepont@hotmail.com>
2023-06-21 22:15:16 -0300

    Projeção dos dados do autor com DTO

---
+++ src/main/java/com/josimarrenepont/workshopmongodb/dto/AuthorDTO.java
@@ -0,0 +1,34 @@
+package com.josimarrenepont.workshopmongodb.dto;
+
+import java.io.Serializable;
+
+import com.josimarrenepont.workshopmongodb.domain.User;
+
+
+public class AuthorDTO implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+
+	private String id;
+	private String name;
+	
+	public AuthorDTO() {
+	}
+	public AuthorDTO(User obj) {
+		id = obj.getId();
+		name = obj.getEmail();
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
