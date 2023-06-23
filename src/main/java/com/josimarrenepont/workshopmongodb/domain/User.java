97770dfa2e3205b1a073e472994f87d539f6623d
jnrenepont@hotmail.com <jnrenepont@hotmail.com>
2023-06-22 21:45:11 -0300

    Referenciando os posts do usuário

--- src/main/java/com/josimarrenepont/workshopmongodb/domain/User.java
+++ src/main/java/com/josimarrenepont/workshopmongodb/domain/User.java
@@ -1,9 +1,12 @@
 package com.josimarrenepont.workshopmongodb.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "user")
@@ -11,14 +14,16 @@ public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	
 	private String id;
 	private String name;
 	private String email;
-
+	
+	@DBRef(lazy = true) //POSTS SÓ SERÃO CARREGADOS EXPLíCITAMENTE APÓS ACESSAR
+	private List<Post> posts = new ArrayList<>();
+	
 	public User() {
 	}
-
+	
 	public User(String id, String name, String email) {
 		super();
 		this.id = id;
@@ -48,6 +53,13 @@ public class User implements Serializable {
 
 	public void setEmail(String email) {
 		this.email = email;
+	}
+	public List<Post> getPosts() {
+		return posts;
+	}
+	
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
 	}
 
 	@Override
